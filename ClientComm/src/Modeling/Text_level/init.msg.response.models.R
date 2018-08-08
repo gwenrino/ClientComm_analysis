031705c57736e46fdccf58b34b43e5c9135f47f2
Gwen Rino <gwenrino@gmail.com>
2018-08-07 17:09:55 -0700

    effect size

--- ClientComm/src/Modeling/Text_level/init.msg.response.models.R
+++ ClientComm/src/Modeling/Text_level/init.msg.response.models.R
@@ -221,6 +221,7 @@ w7 <- glmer(msg_replied_i ~ (1 | PO) + court_date_reminder + appointment_date_re
 summary(w7)
 
 
+
 # Seems like the interaction terms don't really help the model or the explanation. 
 # Choose w2 (no interaction terms)
 
@@ -274,6 +275,16 @@ w13 <- glmer(msg_replied_i ~ (1 | PO) + appointment_date_reminder + court_date_r
              data = initial_msgs_qualities, family = 'binomial', control = glmerControl(optimizer = "bobyqa"))
 summary(w13)
 
+# Try with polite:max_reuse_score and closing:max_reuse_score
+# This switches sign on closing (good) but raises AIC
+w14 <- glmer(msg_replied_i ~ (1 | PO) + court_date_reminder + appointment_date_reminder + 
+               pls_respond + business + max_reuse_score + info + problem + urgency + polite +
+               greeting + closing + yelling + has_client_name + has_user_name + polite:max_reuse_score +
+               closing:max_reuse_score, 
+             data = initial_msgs_qualities, family = 'binomial', control = glmerControl(optimizer = "bobyqa"))
+summary(w14)
+
+
 ## Best model with content and style is w10 AIC 3678.1
 ## Substantially better than content-only model w2 AIC 3722.5
 ## And better than style-only model init_style_mod.2 AIC 3710.4
@@ -284,40 +295,129 @@ summary(w13)
 
 initial_msgs_qualities$send_at_ToD_bins <- relevel(initial_msgs_qualities$send_at_ToD_bins, ref="Morning")
 
-w14 <- glmer(msg_replied_i ~ (1 | PO) + court_date_reminder + appointment_date_reminder + 
+w15 <- glmer(msg_replied_i ~ (1 | PO) + court_date_reminder + appointment_date_reminder + 
                pls_respond + business + max_reuse_score + info + problem + urgency + polite +
                greeting + closing + yelling + has_client_name + has_user_name + polite:max_reuse_score +
                send_at_ToD_bins, 
              data = initial_msgs_qualities, family = 'binomial', control = glmerControl(optimizer = "bobyqa"))
-summary(w14)
+summary(w15)
 # Afternoon is significantly better than morning
 # AIC 3679.7
 
 class(initial_msgs_qualities$send_at_DoW) <- "factor"
 initial_msgs_qualities$send_at_DoW <- relevel(initial_msgs_qualities$send_at_DoW, ref = "Wed")
 
-w15 <- glmer(msg_replied_i ~ (1 | PO) + court_date_reminder + appointment_date_reminder + 
+w16 <- glmer(msg_replied_i ~ (1 | PO) + court_date_reminder + appointment_date_reminder + 
                pls_respond + business + max_reuse_score + info + problem + urgency + polite +
                greeting + closing + yelling + has_client_name + has_user_name + polite:max_reuse_score +
                send_at_DoW, 
              data = initial_msgs_qualities, family = 'binomial', control = glmerControl(optimizer = "bobyqa"))
-summary(w15)
+summary(w16)
 # DoW not significant
 
 initial_msgs_qualities$send_at_hr <- hour(initial_msgs_qualities$send_at)
 
-w16 <- glmer(msg_replied_i ~ (1 | PO) + court_date_reminder + appointment_date_reminder + 
+w17 <- glmer(msg_replied_i ~ (1 | PO) + court_date_reminder + appointment_date_reminder + 
                pls_respond + business + max_reuse_score + info + problem + urgency + polite +
                greeting + closing + yelling + has_client_name + has_user_name + polite:max_reuse_score +
                sin(pi*send_at_hr/12) + cos(pi*send_at_hr/12), 
              data = initial_msgs_qualities, family = 'binomial', control = glmerControl(optimizer = "bobyqa"))
-summary(w16)
+summary(w17)
 # ToD not significant and also difficult to understand. ToD bins are good enough!
 
 ####### Best model #########
-w14 <- glmer(msg_replied_i ~ (1 | PO) + court_date_reminder + appointment_date_reminder + 
+w15 <- glmer(msg_replied_i ~ (1 | PO) + court_date_reminder + appointment_date_reminder + 
                pls_respond + business + max_reuse_score + info + problem + urgency + polite +
                greeting + closing + yelling + has_client_name + has_user_name + polite:max_reuse_score +
                send_at_ToD_bins, 
              data = initial_msgs_qualities, family = 'binomial', control = glmerControl(optimizer = "bobyqa"))
 ############################
+
+## Effect size max_reuse score: up to 18%
+
+init_msgs_max_reuse_lo <- initial_msgs_qualities
+init_msgs_max_reuse_lo$max_reuse_score <- .75
+
+init_msgs_max_reuse_mid <- initial_msgs_qualities
+init_msgs_max_reuse_mid$max_reuse_score <- .88
+
+init_msgs_max_reuse_hi <- initial_msgs_qualities
+init_msgs_max_reuse_hi$max_reuse_score <- 1
+
+predictions_max_reuse_lo <- predict(w15, newdata = init_msgs_max_reuse_lo, type = "response", allow.new.levels=TRUE)
+predictions_max_reuse_mid <- predict(w15, newdata = init_msgs_max_reuse_mid, type = "response", allow.new.levels=TRUE)
+predictions_max_reuse_hi <- predict(w15, newdata = init_msgs_max_reuse_hi, type = "response", allow.new.levels=TRUE)
+
+mean(predictions_max_reuse_lo, na.rm = TRUE) 
+mean(predictions_max_reuse_mid, na.rm = TRUE) 
+mean(predictions_max_reuse_hi, na.rm = TRUE) 
+
+## Effect size business: 25.7%
+
+init_msgs_bus_0 <- initial_msgs_qualities
+init_msgs_bus_0$business <- 0
+
+init_msgs_bus_1 <- initial_msgs_qualities
+init_msgs_bus_1$business <- 1
+
+predictions_bus_0 <- predict(w15, newdata = init_msgs_bus_0, type = "response", allow.new.levels=TRUE)
+predictions_bus_1 <- predict(w15, newdata = init_msgs_bus_1, type = "response", allow.new.levels=TRUE)
+
+mean(predictions_bus_0, na.rm = TRUE)
+mean(predictions_bus_1, na.rm = TRUE)
+
+## Effect size pls_respond: 35.9%
+
+init_msgs_pls_respond_0 <- initial_msgs_qualities
+init_msgs_pls_respond_0$pls_respond <- 0
+
+init_msgs_pls_respond_1 <- initial_msgs_qualities
+init_msgs_pls_respond_1$pls_respond <- 1
+
+predictions_pls_respond_0 <- predict(w15, newdata = init_msgs_pls_respond_0, type = "response", allow.new.levels=TRUE)
+predictions_pls_respond_1 <- predict(w15, newdata = init_msgs_pls_respond_1, type = "response", allow.new.levels=TRUE)
+
+mean(predictions_pls_respond_0, na.rm = TRUE)
+mean(predictions_pls_respond_1, na.rm = TRUE)
+
+## Effect size problem: 26.6%
+
+init_msgs_problem_0 <- initial_msgs_qualities
+init_msgs_problem_0$problem <- 0
+
+init_msgs_problem_1 <- initial_msgs_qualities
+init_msgs_problem_1$problem <- 1
+
+predictions_problem_0 <- predict(w15, newdata = init_msgs_problem_0, type = "response", allow.new.levels=TRUE)
+predictions_problem_1 <- predict(w15, newdata = init_msgs_problem_1, type = "response", allow.new.levels=TRUE)
+
+mean(predictions_problem_0, na.rm = TRUE)
+mean(predictions_problem_1, na.rm = TRUE)
+
+## Effect size urgency: 35.2%
+
+init_msgs_urgency_0 <- initial_msgs_qualities
+init_msgs_urgency_0$urgency <- 0
+
+init_msgs_urgency_1 <- initial_msgs_qualities
+init_msgs_urgency_1$urgency <- 1
+
+predictions_urgency_0 <- predict(w15, newdata = init_msgs_urgency_0, type = "response", allow.new.levels=TRUE)
+predictions_urgency_1 <- predict(w15, newdata = init_msgs_urgency_1, type = "response", allow.new.levels=TRUE)
+
+mean(predictions_urgency_0, na.rm = TRUE)
+mean(predictions_urgency_1, na.rm = TRUE)
+
+## Effect size yelling: 22.5%
+
+init_msgs_yelling_0 <- initial_msgs_qualities
+init_msgs_yelling_0$yelling <- 0
+
+init_msgs_yelling_1 <- initial_msgs_qualities
+init_msgs_yelling_1$yelling <- 1
+
+predictions_yelling_0 <- predict(w15, newdata = init_msgs_yelling_0, type = "response", allow.new.levels=TRUE)
+predictions_yelling_1 <- predict(w15, newdata = init_msgs_yelling_1, type = "response", allow.new.levels=TRUE)
+
+mean(predictions_yelling_0, na.rm = TRUE)
+mean(predictions_yelling_1, na.rm = TRUE)
